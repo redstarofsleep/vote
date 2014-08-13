764629b2cd414e536be2a6fabc122b82516ad734
lhc <redstarofsleep@163.com>
2014-08-13 22:54:59 +0800

    add demo1

--- routes/vote.js
+++ routes/vote.js
@@ -1,3 +1,18 @@
 exports.voteList = function(req, res) {
-
+	var voteList = [
+		{id: 1, title: 'abc'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'},
+		{id: 1, title: 'bcd'}
+	];
+	res.send(voteList);
 };
