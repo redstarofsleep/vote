5c22ffc3857c3078b555a9a2d46ba7aad5881936
lhc <redstarofsleep@163.com>
2014-08-18 22:04:30 +0800

    testsql

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,17 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host : '192.168.18.96',
-	database : 'votedev',
-	user : 'votedev',
-	password : '123456'
+var pool = mysql.createPool({
+	host: '192.168.18.96',
+	database: 'votedev',
+	user: 'votedev',
+	password: '123456'
 });
-// connection.connect();
+
 exports.query = function(sql, param, fun) {
-	
-	connection.query(sql, param, function(err, rows, fields) {
-		fun(rows);
+	pool.getConnection(function(err, connection) {
+		connection.query(sql, param, function(err, rows, fields) {
+			fun(rows);
+			connection.release();
+		});
 	});
-	// connection.end();
 };
